1e74613252e537f9d58f5770bfc2d3b79479c091
4izer0 <ismael.dante89@gmail.com>
2022-02-08 00:40:56 -0600

    question.commit

---
+++ Assets/scrits(taller)/Option.cs
@@ -0,0 +1,12 @@
+[System.Serializable]
+
+public class Option 
+{
+    public string text = null;
+    public bool correct = false;
+
+
+
+
+
+}
---
+++ Assets/scrits(taller)/Question.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour
+{
+    public string text = null;
+    public List<Option>options = null;
+
+
+
+
+
+
+
+
+}
